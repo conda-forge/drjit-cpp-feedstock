91095d391ed69c2ebc7462f7a2f974e552c5e740
Jeongseok Lee <jeongseok@meta.com>
2025-01-15 13:40:09 -0800

    Attempt to set cmake args for python build

--- recipe/build_py.sh
+++ recipe/build_py.sh
@@ -7,4 +7,6 @@ if [[ "${target_platform}" == osx-* ]]; then
   CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
+CMAKE_ARGS="$CMAKE_ARGS -DRJIT_USE_SYSTEM_ROBIN_MAP=ON"
+
 python -m pip install . -vv
