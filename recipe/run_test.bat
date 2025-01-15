676c72318985d79cd50631424c0698fd658dde51
Jeongseok Lee <jeongseok@meta.com>
2025-01-15 10:37:41 -0800

    Add test for building a simple program

---
+++ recipe/run_test.bat
@@ -0,0 +1,10 @@
+@echo on
+
+cmake %SRC_DIR% ^
+  %CMAKE_ARGS% ^
+  -B tests/build ^
+  -DBUILD_SHARED_LIBS=ON
+if errorlevel 1 exit 1
+
+cmake --build build --parallel --config Release
+if errorlevel 1 exit 1
