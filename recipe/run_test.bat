49e79462775138b0a8a861f3650cd7fd240b8dd3
Jeongseok Lee <jeongseok@meta.com>
2025-05-05 18:29:51 -0700

    Build example conditionally

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -3,7 +3,8 @@
 cmake tests ^
   %CMAKE_ARGS% ^
   -B tests/build ^
-  -DBUILD_SHARED_LIBS=ON
+  -DBUILD_SHARED_LIBS=ON ^
+  -DENABLE_LLVM=ON
 if errorlevel 1 exit 1
 
 cmake --build tests/build --parallel --config Release
