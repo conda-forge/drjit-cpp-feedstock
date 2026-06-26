1da9acea3c9cda52dd054b8da712ae3fe2ce5f3c
Jeongseok Lee <jeongseok@meta.com>
2026-06-26 14:43:39 -0700

    Simplify Dr.Jit LLVM packaging

--- recipe/run_test_cpp.bat
+++ recipe/run_test_cpp.bat
@@ -10,7 +10,7 @@ cmake tests ^
   %CMAKE_ARGS% ^
   -B tests/build ^
   -DBUILD_SHARED_LIBS=ON ^
-  -DENABLE_LLVM=ON ^
+  -DENABLE_LLVM=OFF ^
   -DENABLE_CUDA=%ENABLE_CUDA% ^
 if errorlevel 1 exit 1
 
