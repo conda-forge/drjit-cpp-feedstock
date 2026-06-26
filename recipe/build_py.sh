afe01f7d15fcbb5eaba8e8f549f0bb80e0daea08
Jeongseok Lee <jeongseok@meta.com>
2026-06-26 11:31:32 -0700

    Update Dr.Jit submodule sources for 1.4.0

--- recipe/build_py.sh
+++ recipe/build_py.sh
@@ -7,6 +7,12 @@ if [[ "${target_platform}" == osx-* ]]; then
   CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
+if [[ "${cuda_compiler_version:-None}" != "None" ]]; then
+  export CMAKE_ARGS="${CMAKE_ARGS:-} -DDRJIT_ENABLE_CUDA=ON -DDRJIT_ENABLE_OPTIX=ON"
+else
+  export CMAKE_ARGS="${CMAKE_ARGS:-} -DDRJIT_ENABLE_CUDA=OFF -DDRJIT_ENABLE_OPTIX=OFF"
+fi
+
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
   export CMAKE_ARGS="${CMAKE_ARGS:-} -DDRJIT_ENABLE_STUBS=OFF"
 else
